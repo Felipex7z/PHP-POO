7aab51b9b1b04bbbd4b6ae34adc9f1b5375a8979
Luiz Felipe Cordeiro <114431500+Felipex7z@users.noreply.github.com>
2024-01-05 19:02:53 -0300

    cmt

--- exercicios/class.caneta.php
+++ exercicios/class.caneta.php
@@ -2,29 +2,38 @@
 class Caneta{
     public $modelo;
     private $ponta;
+    private $cor;
+
+    public function __construct(){
+        $this->cor = 'verde';
+
+
+    }
+    public function getModelo(){
+        return $this->modelo;
+    }
+    public function setModelo($m){
+       $this->modelo = $m;
+    }
+
+    public function getponta(){
+       return $this->ponta;
+    }
+    public function setPonta($p){
+       $this->ponta = $p;
+
+    }
+
+        
+    }
  
-     public function getModelo(){
-         return $this->modelo;
-     }
-     public function setModelo($m){
-        $this->modelo = $m;
-     }
-
-     public function getponta(){
-        return $this->ponta;
-     }
-     public function setPonta($p){
-        $this->ponta = $p;
-
-     }
+     
 
 
 
 
 
-
-
-    }  
+     
     
     
 
